aa776cf445cd7589a5bda38a67ec685880ef29aa
FelixAL07 <lerdahl07@gmail.com>
2025-03-26 23:42:34 +0100

    Secure

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBIdPdJ99vBAPu7dm2kc9uQGf8-ngzmyg4",
-    authDomain: "case-vgs-utvikling-felix.firebaseapp.com",
-    projectId: "case-vgs-utvikling-felix",
-    storageBucket: "case-vgs-utvikling-felix.firebasestorage.app",
-    messagingSenderId: "774382388824",
-    appId: "1:774382388824:web:8fe7db0ca883deb2a7ec2d"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
